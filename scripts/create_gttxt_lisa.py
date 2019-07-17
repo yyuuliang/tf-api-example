9b55d332c4b35d8be8bb53b563e3b5a26418205b
yyuuliang <yulianl@g.clemson.edu>
2019-07-16 21:59:31 -0700

    fix a bug that may cause file lost

--- scripts/create_gttxt_lisa.py
+++ scripts/create_gttxt_lisa.py
@@ -19,26 +19,29 @@ def csv_txt():
     csv_fname = os.path.join('dataset/lisa/train.csv')
     idx = 0
     gt2 = open('dataset/lisa/gt.txt','w') 
-    fidx = 0
+    fidx = -1
     last_img_name = ''
     with open(csv_fname, 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|', )
+        # remove the first line from lisa' csv file
         for item in spamreader:
-            if fidx > 0:
-                row = item[0].split(';')
-                img_name = row[0].split('--')[1]
-                # copy the image to project folder using new name
-                img_path = '/home/notus/whitebase/Dataset/LISA-traffic-lights/dayTrain/dayClip1/frames/dayClip1--'+img_name
-                im = cv2.imread(img_path)
-                cv2.imwrite('dataset/lisa/'+img_name, im)
-                xmin = int(row[2])
-                ymin = int(row[3])
-                xmax = int(row[4])
-                ymax = int(row[5])
-                class_id = '1'
-                gtstr = '{};{};{};{};{};{}\n'.format(img_name,xmin,ymin,xmax,ymax,class_id)
-                gt2.write(gtstr)
-            fidx = fidx +1
+            row = item[0].split(';')
+            img_name = row[0].split('--')[1]
+            if last_img_name !=  img_name:
+                last_img_name = img_name
+                fidx = fidx +1
+            img_name_idx = str(fidx).zfill(5)
+            # copy the image to project folder using new name
+            img_path = '/home/notus/whitebase/Dataset/LISA-traffic-lights/dayTrain/dayClip1/frames/dayClip1--'+img_name
+            im = cv2.imread(img_path)
+            cv2.imwrite('dataset/lisa/'+img_name_idx+'.jpg', im)
+            xmin = int(row[2])
+            ymin = int(row[3])
+            xmax = int(row[4])
+            ymax = int(row[5])
+            class_id = '1'
+            gtstr = '{}.jpg;{};{};{};{};{}\n'.format(img_name_idx,xmin,ymin,xmax,ymax,class_id)
+            gt2.write(gtstr)
     gt2.close()
 
 
